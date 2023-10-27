f027a38c46ef1bd079858d8a6cd143da6ed4d8df
isaacmoyam <isaacmoyamayordomo.guadalupe@fundacionloyola.net>
2023-10-27 15:41:13 +0200

    enviar variables con enlaces

--- controladores/lugar/lugar_opciones_form.php
+++ controladores/lugar/lugar_opciones_form.php
@@ -14,45 +14,58 @@
         try {
             $conexion = new Conexion(); // Crea un objeto de la clase Conexion para establecer la conexión a la base de datos.
             $objeto = new Lugar($conexion->conexion); // Crea un objeto de la clase Lugar y pasa la conexión como parámetro.
+            $opcion = $_GET['opcion']; // Recoge la opcion enviada por enlace.
 
-            if (isset($_GET['insert'])) {
-                // Si se recibe un parámetro "insert" muestra un formulario para agregar un nuevo lugar.
-                echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
-                echo '<label for="ip">Ip:</label>';
-                echo '<input type="text" name="ip"<br>';
-                echo '<label for="lugar">Lugar:</label>';
-                echo '<input type="text" name="lugar"<br>';
-                echo '<label for="desc">Descripción:</label>';
-                echo '<input type="text" name="desc"><br>';
-                echo '<input type="hidden" value="insert" name="opcion"><br>';
-                echo '<input type="submit">';
-                echo '</form>';
+            switch($opcion){
+                // Si se recibe "insert" muestra un formulario para agregar un nuevo lugar.
+                case 'insert':
+                    echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
+                    echo '<label for="ip">Ip:</label>';
+                    echo '<input type="text" name="ip"<br>';
+                    echo '<label for="lugar">Lugar:</label>';
+                    echo '<input type="text" name="lugar"<br>';
+                    echo '<label for="desc">Descripción:</label>';
+                    echo '<input type="text" name="desc"><br>';
+                    echo '<input type="hidden" value="insert" name="opcion"><br>';
+                    echo '<input type="submit">';
+                    echo '</form>';
+                    echo "<br>";
+                    echo "<br>";
+                    echo "<a href='../../vistas/crud_lugar.html'>Volver</a>";
+                    break;
+                case 'select':
+                    // Si se recibe "select" llama al método "listar" del objeto para mostrar la lista de lugares.
+                    $objeto->listar();
+                    break;
+                case 'update':
+                    // Si se recibe "update" muestra un formulario para actualizar un lugar.
+                    echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
+                    echo '<label for="ip">Ip:</label>';
+                    echo '<input type="text" name="ip"><br>';
+                    echo '<input type="hidden" value="update" name="opcion"><br>';
+                    echo '<input type="submit">';
+                    echo '</form>';
+                    echo "<br>";
+                    echo "<br>";
+                    echo "<a href='../../vistas/crud_lugar.html'>Volver</a>";
+                    break;
+                case 'delete':
+                    // Si se recibe "delete" muestra un formulario para eliminar un lugar.
+                    echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
+                    echo '<label for="nombre">Ip:</label>';
+                    echo '<input type="text" name="ip"<br>';
+                    echo '<input type="hidden" value="delete" name="opcion"><br><br>';
+                    echo '<input type="submit">';
+                    echo '</form>';
+                    echo "<br>";
+                    echo "<br>";
+                    echo "<a href='../../vistas/crud_lugar.html'>Volver</a>";
+                    break;
+                default:
+                    echo "Esa opción no existe";
+                    break;
             }
 
-            if (isset($_GET['select'])) {
-                // Si se recibe un parámetro "select" llama al método "listar" del objeto para mostrar la lista de lugares.
-                $objeto->listar();
-            }
-
-            if (isset($_GET['update'])) {
-                // Si se recibe un parámetro "update" muestra un formulario para actualizar un lugar.
-                echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
-                echo '<label for="ip">Ip:</label>';
-                echo '<input type="text" name="ip"><br>';
-                echo '<input type="hidden" value="update" name="opcion"><br>';
-                echo '<input type="submit">';
-                echo '</form>';
-            }
-
-            if (isset($_GET['delete'])) {
-                // Si se recibe un parámetro "delete" muestra un formulario para eliminar un lugar.
-                echo '<br><form method="POST" action="lugar_procesar_formulario.php">';
-                echo '<label for="nombre">Ip:</label>';
-                echo '<input type="text" name="ip"<br>';
-                echo '<input type="hidden" value="delete" name="opcion"><br><br>';
-                echo '<input type="submit">';
-                echo '</form>';
-            }
         } catch (mysqli_sql_exception $e) {
             // Muestra un mensaje de error en caso de pérdida de conexión con la base de datos.
             echo "No se pudo conectar con la base de datos";
